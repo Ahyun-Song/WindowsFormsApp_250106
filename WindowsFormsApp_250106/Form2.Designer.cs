ec6e81c779e9b545b833e92ac1e2b2e33ef320dc
Ahyun-Song <yoon9113@naver.com>
2025-01-06 11:56:59 +0900

    Form2_Directory/Extension

---
+++ WindowsFormsApp_250106/Form2.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WindowsFormsApp_250106
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_directory = new System.Windows.Forms.TextBox();
+            this.button_search = new System.Windows.Forms.Button();
+            this.listBox_files = new System.Windows.Forms.ListBox();
+            this.textBox_extension = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_directory
+            // 
+            this.textBox_directory.Location = new System.Drawing.Point(74, 66);
+            this.textBox_directory.Name = "textBox_directory";
+            this.textBox_directory.Size = new System.Drawing.Size(377, 28);
+            this.textBox_directory.TabIndex = 0;
+            // 
+            // button_search
+            // 
+            this.button_search.Location = new System.Drawing.Point(477, 66);
+            this.button_search.Name = "button_search";
+            this.button_search.Size = new System.Drawing.Size(84, 83);
+            this.button_search.TabIndex = 1;
+            this.button_search.Text = "Search";
+            this.button_search.UseVisualStyleBackColor = true;
+            this.button_search.Click += new System.EventHandler(this.button_search_Click);
+            // 
+            // listBox_files
+            // 
+            this.listBox_files.FormattingEnabled = true;
+            this.listBox_files.ItemHeight = 18;
+            this.listBox_files.Location = new System.Drawing.Point(74, 166);
+            this.listBox_files.Name = "listBox_files";
+            this.listBox_files.Size = new System.Drawing.Size(487, 238);
+            this.listBox_files.TabIndex = 2;
+            // 
+            // textBox_extension
+            // 
+            this.textBox_extension.Location = new System.Drawing.Point(74, 121);
+            this.textBox_extension.Name = "textBox_extension";
+            this.textBox_extension.Size = new System.Drawing.Size(377, 28);
+            this.textBox_extension.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Directory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Extension";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_extension);
+            this.Controls.Add(this.listBox_files);
+            this.Controls.Add(this.button_search);
+            this.Controls.Add(this.textBox_directory);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_directory;
+        private System.Windows.Forms.Button button_search;
+        private System.Windows.Forms.ListBox listBox_files;
+        private System.Windows.Forms.TextBox textBox_extension;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ WindowsFormsApp_250106/Form2.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_250106
+{
+    public partial class Form2 : Form
+    {
+        private BackgroundWorker backgroundWorker;
+
+        public Form2()
+        {
+            InitializeComponent();
+            InitializeBackgroundWorker();
+        }
+
+        private void InitializeBackgroundWorker()
+        {
+            backgroundWorker = new BackgroundWorker();
+            backgroundWorker.WorkerReportsProgress = false;
+            backgroundWorker.WorkerSupportsCancellation = true;
+            backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+        }
+
+        private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            button_search.Enabled = true;
+
+            if (e.Result is string errorMessage)
+            {
+                MessageBox.Show($"An error occurred: {errorMessage}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (e.Result is System.Collections.Generic.List<string> files)
+            {
+                listBox_files.Items.AddRange(files.ToArray());
+                if (files.Count == 0)
+                {
+                    MessageBox.Show("No files found with the specified extension.", "No Files Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var (directory, extension) = ((string, string))e.Argument;
+
+            try
+            {
+                var files = Directory.EnumerateFiles(directory, $"*.{extension}", SearchOption.AllDirectories).ToList();
+                e.Result = files;
+            }
+            catch (Exception ex)
+            {
+                e.Result = ex.Message;
+            }
+        }
+
+        private void button_search_Click(object sender, EventArgs e)
+        {
+            string directory = textBox_directory.Text;
+            string extension = textBox_extension.Text;
+
+            if (string.IsNullOrWhiteSpace(directory) || string.IsNullOrWhiteSpace(extension))
+            {
+                MessageBox.Show("Please provide both directory and file extension.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!Directory.Exists(directory))
+            {
+                MessageBox.Show("The specified directory does not exist.", "Directory Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            listBox_files.Items.Clear();
+            button_search.Enabled = false;
+            backgroundWorker.RunWorkerAsync((directory, extension));
+        }
+    }
+}
